73c404f43c5447fdb4a37f498977a1b84f647054
Mbaduko <nsengiyumvaclement247@gmail.com>
2023-12-15 19:53:16 +0200

    Creating files needed in drawer navigation

---
+++ screens/DrawerContent.js
@@ -0,0 +1,10 @@
+import { View, Text ,StyleSheet} from "react-native";
+
+const Menu = () => {
+    return (
+        <View style={StyleSheet.create({alignItems: 'center', justifyContent: 'center'})}>
+            <Text>MENUS</Text>
+        </View>
+    );
+}
+export default Menu;
